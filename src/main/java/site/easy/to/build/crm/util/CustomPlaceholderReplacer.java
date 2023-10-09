ed870669fd27a41ded76b7cdb4fbdf7ddfc517ed
Ahmed Samir <54330098+wp-ahmed@users.noreply.github.com>
2023-10-10 01:43:01 +0300

    Update CustomPlaceholderReplacer.java

--- src/main/java/site/easy/to/build/crm/util/CustomPlaceholderReplacer.java
+++ src/main/java/site/easy/to/build/crm/util/CustomPlaceholderReplacer.java
@@ -1,1 +1,23 @@
+package site.easy.to.build.crm.util;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class CustomPlaceholderReplacer {
+
+    private final String databaseName;
+
+    public CustomPlaceholderReplacer(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public void executeScriptWithPlaceholderReplacement(String scriptPath) throws IOException {
+        ClassPathResource resource = new ClassPathResource(scriptPath);
+        byte[] bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
+        String scriptContent = new String(bytes, StandardCharsets.UTF_8);
+        String replacedScriptContent = scriptContent.replace("@database.name@", databaseName);
+    }
+}
