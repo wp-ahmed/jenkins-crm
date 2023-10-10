b16c1749bfff36471b3d54407100960e30c52629
Ahmed Samir <54330098+wp-ahmed@users.noreply.github.com>
2023-10-10 08:27:58 +0300

    Update CustomPlaceholderReplacer.java

--- src/main/java/site/easy/to/build/crm/util/CustomPlaceholderReplacer.java
+++ src/main/java/site/easy/to/build/crm/util/CustomPlaceholderReplacer.java
@@ -3,7 +3,9 @@ package site.easy.to.build.crm.util;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.FileCopyUtils;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 public class CustomPlaceholderReplacer {
@@ -19,5 +21,22 @@ public class CustomPlaceholderReplacer {
         byte[] bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
         String scriptContent = new String(bytes, StandardCharsets.UTF_8);
         String replacedScriptContent = scriptContent.replace("@database.name@", databaseName);
+
+        String outputFilePath = "src/main/resources/schema.sql";
+        try (Writer writer = new FileWriter(outputFilePath)) {
+            writer.write(replacedScriptContent);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Usage: java CustomPlaceholderReplacer <databaseName> <scriptPath>");
+        }
+
+        String databaseName = args[0];
+        String scriptPath = args[1];
+
+        CustomPlaceholderReplacer placeholderReplacer = new CustomPlaceholderReplacer(databaseName);
+        placeholderReplacer.executeScriptWithPlaceholderReplacement(scriptPath);
     }
 }
